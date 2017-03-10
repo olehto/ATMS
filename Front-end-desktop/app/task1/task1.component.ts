b5af6a010d1ab5650d184aeb3dbeacf215a6d2a9
SERP3NT <srp3nt@yandex.ru>
2017-03-10 16:36:16 +0200

    Added initials front-end workflow.

---
+++ Front-end-desktop/app/task1/task1.component.ts
@@ -0,0 +1,33 @@
+/**
+ * Created by Serpent on 09.03.2017.
+ */
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../_models/index';
+import { UserService } from '../_services/index';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'task1.component.html'
+})
+
+export class Task1Component implements OnInit {
+    currentUser: User;
+    users: User[] = [];
+
+    constructor(private userService: UserService) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    }
+
+    ngOnInit() {
+        this.loadAllUsers();
+    }
+
+    deleteUser(id: number) {
+        this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
+    }
+
+    private loadAllUsers() {
+        this.userService.getAll().subscribe(users => { this.users = users; });
+    }
+}
