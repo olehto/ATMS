89a12f38903416e247f79d7ea638c5bbc30da84d
Alex Kazanovskiy <alex.kazanovskiy@gmail.com>
2017-03-30 14:43:26 +0300

    Fix: user registration

---
+++ Back-end/atms/src/main/java/com/atms/controller/LoginController.java
@@ -0,0 +1,40 @@
+package com.atms.controller;
+
+import com.atms.model.Developer;
+import com.atms.service.DeveloperService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+/**
+ * @author Alex Kazanovskiy.
+ */
+
+@RestController
+public class LoginController {
+
+
+    private final DeveloperService developerService;
+
+    @Autowired
+    public LoginController(DeveloperService developerService) {
+        this.developerService = developerService;
+    }
+
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public ResponseEntity<Developer> registration(@Valid Developer developer) {
+        Developer newDeveloper;
+        try {
+            newDeveloper = developerService.save(developer);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(newDeveloper, HttpStatus.OK);
+    }
+}
