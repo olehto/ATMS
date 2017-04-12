7b8e769057785138aacfade83ad64fda53536f9e
Alex Kazanovskiy <alex.kazanovskiy@gmail.com>
2017-04-12 17:10:35 +0300

    Fix: login with email

--- Back-end/atms/src/main/java/com/atms/service/impl/DeveloperDetailsServiceImpl.java
+++ Back-end/atms/src/main/java/com/atms/service/impl/DeveloperDetailsServiceImpl.java
@@ -1,12 +1,13 @@
 package com.atms.service.impl;
 
 import com.atms.model.Developer;
-import com.atms.service.DeveloperService;
+import com.atms.repository.DeveloperRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.client.resource.OAuth2AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,66 +21,31 @@ import java.util.Collection;
 @Service
 public class DeveloperDetailsServiceImpl implements UserDetailsService {
 
-    private final DeveloperService developerService;
+    private final DeveloperRepository developerRepository;
 
     @Autowired
-    public DeveloperDetailsServiceImpl(DeveloperService developerService) {
-        this.developerService = developerService;
+    public DeveloperDetailsServiceImpl(DeveloperRepository developerService) {
+        this.developerRepository = developerService;
     }
 
     @Override
     @Transactional
     public UserDetails loadUserByUsername(final String login) {
-        Developer userFromDatabase = developerService.findByUsername(login);
-        if (userFromDatabase == null) {
-            userFromDatabase = developerService.findByEmail(login);
-        }
+        Developer userFromDatabase = developerRepository.findByNickname(login);
+
+        if (userFromDatabase == null)
+            userFromDatabase = developerRepository.findByEmail(login);
+
+        if (userFromDatabase.isLocked())
+            throw new OAuth2AccessDeniedException("Account is locked");
+
         Collection<GrantedAuthority> grantedAuthorities = new ArrayList<>();
         for (GrantedAuthority authority : userFromDatabase.getAuthorities()) {
             GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(authority.getAuthority());
             grantedAuthorities.add(grantedAuthority);
         }
-        return new org.springframework.security.core.userdetails.User(userFromDatabase.getNickname(), userFromDatabase.getPassword(), grantedAuthorities);
+        return new org.springframework.security.core.userdetails.User(userFromDatabase.getNickname(),
+                userFromDatabase.getPassword(), grantedAuthorities);
 
     }
-
-    private final static class UserRepositoryUserDetails extends Developer implements UserDetails {
-
-        private static final long serialVersionUID = 1L;
-
-        private UserRepositoryUserDetails(Developer developer) {
-            super(developer);
-        }
-
-        @Override
-        public Collection<? extends GrantedAuthority> getAuthorities() {
-            return getAuthorities();
-        }
-
-        @Override
-        public String getUsername() {
-            return getNickname();
-        }
-
-        @Override
-        public boolean isAccountNonExpired() {
-            return true;
-        }
-
-        @Override
-        public boolean isAccountNonLocked() {
-            return true;
-        }
-
-        @Override
-        public boolean isCredentialsNonExpired() {
-            return true;
-        }
-
-        @Override
-        public boolean isEnabled() {
-            return true;
-        }
-    }
-
 }
